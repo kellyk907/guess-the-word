f74f2a3a717857f6ef70bbd5d53107cf201499f6
kelly_k <pinkflamingo681@gmail.com>
2024-11-10 15:48:23 -0500

    select elements and add placeholders

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//The unordered list where the player’s guessed letters will appear.
+const guessedLettersElement = document.querySelector(".guessed-letters");
+//The button with the text “Guess!” in it.
+const guessedLetterButton = document.querySelector(".guess");
+//The text input where the player will guess a letter.
+const letterInput = document.querySelector(".letter");
+//The empty paragraph where the word in progress will appear.
+const wordInProgress = document.querySelector(".word-in-progress");
+//The paragraph where the remaining guesses will display.
+const remainingGuessesElement = document.querySelector(".remaining");
+//The span inside the paragraph where the remaining guesses will display.
+const remainingGuessesSpan = document.querySelector(".remaining span");
+//The empty paragraph where messages will appear when the player guesses a letter.
+const message = document.querySelector(".message");
+//The hidden button that will appear prompting the player to play again.
+const playAgainButton = document.querySelector(".play-again");
+//starting word
+const word = "magnolia";
+
+//display symbols while waiting for letter guesses
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+      console.log(letter);
+      placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+  };
+
+  placeholder(word);
+
+  //Event listener for the button
+  guessedLetterButton.addEventListener("click",function(e){
+e.preventDefault();
+const guess = letterInput.value;
+console.log(guess);
+letterInput.value = "";
+  });
